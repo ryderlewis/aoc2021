613aa30932e8e04a34331c14d7b8da8d8bd35b1e
Ryder Lewis <ryder@stripe.com>
2021-12-23 11:53:16 -0500

    refactor some stuff for day23

--- pkg/day23/day23.go
+++ pkg/day23/day23.go
@@ -5,10 +5,11 @@ import (
 	"fmt"
 	"github.com/ryderlewis/aoc2021/pkg/challenge"
 	"io"
+	"math"
 	"strconv"
 )
 
-type Amphipod rune
+type Amphipod byte
 
 func (a Amphipod) String() string {
 	if a == EMPTY {
@@ -26,26 +27,114 @@ const (
 	D = Amphipod('D')
 )
 
-type Room struct {
+type State [19]Amphipod
+
+type Move struct {
+	amphipod Amphipod
+	from, to, distance, energy int
+}
+
+type AmphipodData struct {
 	hallIndex int
-	amphipods [2]Amphipod
+	destIndexes []int
+	energy int
 }
 
 type Runner struct {
-	hall [11]Amphipod
-	rooms [4]Room
+	startingState State
+	amphipodData map[Amphipod]*AmphipodData
 }
 
-var _ challenge.DailyChallenge = &Runner{}
+// leastEnergy tries a naive recursive implementation, given the current state,
+// find the minimum number of moves to get to a final state
+func (r *Runner) leastEnergy(s *State) int {
+	if r.isFinal(s) {
+		return 0
+	}
+
+	newState := *s // makes a copy of s
+	bestEnergy := math.MaxInt
+	for i, _ := range s {
+		// find legal moves for amphipod a, at position i
+		for _, move := range r.validMoves(s, i) {
+			// swap values
+			newState[move.from], newState[move.to] = newState[move.to], newState[move.from]
+
+			energy := move.energy + r.leastEnergy(&newState)
+			if energy < bestEnergy {
+				bestEnergy = energy
+			}
+
+			// swap back
+			newState[move.from], newState[move.to] = newState[move.to], newState[move.from]
+		}
+	}
+
+	return bestEnergy
+}
+
+func (r *Runner) validMoves(s *State, i int) []*Move {
+	if s[i] == EMPTY {
+		return nil
+	}
+
+	a := s[i]
+	data := r.amphipodData[s[i]]
+
+	var hallBlocked [11]bool
+	var moves []*Move
+
+	if 0 <= i && i <= 10 {
+		// amphipod is in the hall. only legal moves are to go to its final destination
+		var dest int
+		if s[data.destIndexes[1]] == EMPTY && s[data.destIndexes[0]] == EMPTY {
+			dest = data.destIndexes[1]
+		} else if s[data.destIndexes[1]] == a && s[data.destIndexes[0]] == EMPTY {
+			dest = data.destIndexes[0]
+		} else {
+			// no legal move
+			return nil
+		}
+
+		// see if there's a clear path from the current location to the destination index
+		move := r.move(s, data, i, dest)
+		if move != nil {
+			moves = append(moves, move)
+		}
+	} else if 11 <= i && i <= 14 {
+		// amphipod is in a room closest to the hall. see if it can move out of the room
+		b := s[i+4] // other amphipod in the same room
+		if a == b && data.destIndexes[0] == i {
+			// amphipod is in its final destination
+			return nil
+		}
+
+		// amphipod can move to let out its partner, or go to its final home.
+		// Note that to do this, we need to see which way this amphipod should
+		// move so that it doesn't block any other amphipods in the hall from
+		// moving to their destination.
+	}
+
+	return moves
+}
+
+func (r *Runner) move(s *State, data *AmphipodData, from, to int) *Move {
+	return nil
+}
+
+func (r *Runner) isFinal(s *State) bool {
+	return s[11] == A && s[15] == A &&
+		s[12] == B && s[16] == B &&
+		s[13] == C && s[17] == C &&
+		s[14] == D && s[18] == D
+}
 
 func (r *Runner) Challenge1(input io.Reader) (string, error) {
 	if err := r.readInput(input); err != nil {
 		return "", err
 	}
 
-	r.print()
-
-	return strconv.Itoa(0), nil
+	return strconv.Itoa(r.leastEnergy(&r.startingState)), nil
 }
 
 func (r *Runner) Challenge2(input io.Reader) (string, error) {
@@ -56,40 +145,8 @@ func (r *Runner) Challenge2(input io.Reader) (string, error) {
 	return strconv.Itoa(0), nil
 }
 
-func (r *Runner) print() {
-/*
-   #############
-   #...........#
-   ###B#C#B#D###
-     #A#D#C#A#
-     #########
-*/
-	fmt.Println("#############")
-	fmt.Print("#")
-	for _, a := range r.hall {
-		fmt.Printf("%s", a)
-	}
-	fmt.Println("#")
-
-	fmt.Print("###")
-	for _, room := range r.rooms {
-		fmt.Printf("%s#", room.amphipods[0])
-	}
-	fmt.Println("##")
-
-	fmt.Print("  #")
-	for _, room := range r.rooms {
-		fmt.Printf("%s#", room.amphipods[1])
-	}
-	fmt.Println()
-	fmt.Println("  #########")
-}
-
 func (r *Runner) readInput(input io.Reader) error {
-	r.rooms[0].hallIndex = 2
-	r.rooms[1].hallIndex = 4
-	r.rooms[2].hallIndex = 6
-	r.rooms[3].hallIndex = 8
+	r.initialize()
 
 	scanner := bufio.NewScanner(input)
 	scanner.Scan()
@@ -97,17 +154,73 @@ func (r *Runner) readInput(input io.Reader) error {
 
 	scanner.Scan()
 	line := scanner.Text()
-	r.rooms[0].amphipods[0] = Amphipod(line[3])
-	r.rooms[1].amphipods[0] = Amphipod(line[5])
-	r.rooms[2].amphipods[0] = Amphipod(line[7])
-	r.rooms[3].amphipods[0] = Amphipod(line[9])
+	r.startingState[11] = Amphipod(line[3])
+	r.startingState[12] = Amphipod(line[5])
+	r.startingState[13] = Amphipod(line[7])
+	r.startingState[14] = Amphipod(line[9])
 
 	scanner.Scan()
 	line = scanner.Text()
-	r.rooms[0].amphipods[1] = Amphipod(line[3])
-	r.rooms[1].amphipods[1] = Amphipod(line[5])
-	r.rooms[2].amphipods[1] = Amphipod(line[7])
-	r.rooms[3].amphipods[1] = Amphipod(line[9])
+	r.startingState[15] = Amphipod(line[3])
+	r.startingState[16] = Amphipod(line[5])
+	r.startingState[17] = Amphipod(line[7])
+	r.startingState[18] = Amphipod(line[9])
 
 	return nil
 }
+
+func (r *Runner) initialize() {
+	r.amphipodData = map[Amphipod]*AmphipodData{
+		A: {
+			hallIndex: 2,
+			destIndexes: []int{11, 15},
+			energy: 1,
+		},
+		B: {
+			hallIndex: 4,
+			destIndexes: []int{12, 16},
+			energy: 10,
+		},
+		C: {
+			hallIndex: 6,
+			destIndexes: []int{13, 17},
+			energy: 100,
+		},
+		C: {
+			hallIndex: 8,
+			destIndexes: []int{14, 18},
+			energy: 1000,
+		},
+	}
+}
+
+func (s State) print() {
+	/*
+	   #############
+	   #...........#
+	   ###B#C#B#D###
+	     #A#D#C#A#
+	     #########
+	*/
+	fmt.Println("#############")
+	fmt.Print("#")
+	for _, a := range s[:11] {
+		fmt.Printf("%s", a)
+	}
+	fmt.Println("#")
+
+	fmt.Print("###")
+	for _, a := range s[11:15] {
+		fmt.Printf("%s#", a)
+	}
+	fmt.Println("##")
+
+	fmt.Print("  #")
+	for _, a := range s[15:] {
+		fmt.Printf("%s#", a)
+	}
+	fmt.Println()
+	fmt.Println("  #########")
+}
+
+var _ challenge.DailyChallenge = &Runner{}
